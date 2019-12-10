6552994b41146f256f607912eb5b720aac2bbd7f
Azure Pipelines <azure@microsoft.com>
2019-12-10 00:23:27 +0000

    Deploy code docs to GitHub Pages from Azure Pipelines
    Azure Pipelines build: 20191210.1
    
    Commit: 0bb026201f45251ba59ae3325db562350eb9e628

--- docs/stage/24/group__rdmnet__controller__cpp.js
+++ docs/stage/24/group__rdmnet__controller__cpp.js
@@ -6,7 +6,7 @@ var group__rdmnet__controller__cpp =
       [ "HandleConnectedToBroker", "classrdmnet_1_1_controller_notify_handler.html#a4397ef34f6f5bceaa91b7d58e39915d7", null ],
       [ "HandleDisconnectedFromBroker", "classrdmnet_1_1_controller_notify_handler.html#a0b697c81a585e4112380416c0df43e54", null ],
       [ "HandleLlrpRdmCommand", "classrdmnet_1_1_controller_notify_handler.html#a712a82dd20f02cc607b45f8754a3c47c", null ],
-      [ "HandleRdmCommand", "classrdmnet_1_1_controller_notify_handler.html#a26fc7abc8e467fe70b59d16ed66c7cc3", null ],
+      [ "HandleRdmCommand", "classrdmnet_1_1_controller_notify_handler.html#a00c04a4fd3648bda49036abba3f9dad9", null ],
       [ "HandleRdmResponse", "classrdmnet_1_1_controller_notify_handler.html#a174dab015e4866cbe50fd56101c50f71", null ],
       [ "HandleRptStatus", "classrdmnet_1_1_controller_notify_handler.html#a248bff7576d51972f24658f8d1f46711", null ]
     ] ],
